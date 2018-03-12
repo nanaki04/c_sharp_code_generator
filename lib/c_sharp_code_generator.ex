5edc297923f149bfffc4f22e6dd9c0905945b72f
zwetsloot robert jan <zwetsloot.robert.jan@techcross.co.jp>
2018-03-13 08:10:05 +0900

    made options overridable

--- lib/c_sharp_code_generator.ex
+++ lib/c_sharp_code_generator.ex
@@ -18,36 +18,36 @@ defmodule CSharpCodeGenerator do
   @unity_class_template File.read! "templates/custom/unity_class.tmpl"
 
   @spec generate(code_parser_state, options) :: :ok
-  def generate(code_parser_state, _options \\ []) do
-    BoilerplateGenerator.generate(code_parser_state,
-      extension: ".cs",
-      class_template: @class_template,
-      enum_template: @enum_template,
-      enum_property_template: @enum_property_template,
-      interface_template: @interface_template,
-      interface_method_template: @interface_method_template,
-      interface_property_template: @interface_property_template,
-      method_template: @method_template,
-      method_parameter_doc_template: @method_parameter_doc_template,
-      public_property_template: @public_property_template,
-      private_property_template: @private_property_template
+  def generate(code_parser_state, options \\ []) do
+    BoilerplateGenerator.generate(code_parser_state, options
+      |> Keyword.put_new(:extension, ".cs")
+      |> Keyword.put_new(:class_template, @class_template)
+      |> Keyword.put_new(:enum_template, @enum_template)
+      |> Keyword.put_new(:enum_property_template, @enum_property_template)
+      |> Keyword.put_new(:interface_template, @interface_template)
+      |> Keyword.put_new(:interface_method_template, @interface_method_template)
+      |> Keyword.put_new(:interface_property_template, @interface_property_template)
+      |> Keyword.put_new(:method_template, @method_template)
+      |> Keyword.put_new(:method_parameter_doc_template, @method_parameter_doc_template)
+      |> Keyword.put_new(:public_property_template, @public_property_template)
+      |> Keyword.put_new(:private_property_template, @private_property_template)
     )
   end
 
   @spec generate_for_unity(code_parser_state, options) :: :ok
-  def generate_for_unity(code_parser_state, _options \\ []) do
-    BoilerplateGenerator.generate(code_parser_state,
-      extension: ".cs",
-      class_template: @unity_class_template,
-      enum_template: @enum_template,
-      enum_property_template: @enum_property_template,
-      interface_template: @interface_template,
-      interface_method_template: @interface_method_template,
-      interface_property_template: @interface_property_template,
-      method_template: @method_template,
-      method_parameter_doc_template: @method_parameter_doc_template,
-      public_property_template: @public_property_template,
-      private_property_template: @unity_private_property_template
+  def generate_for_unity(code_parser_state, options \\ []) do
+    BoilerplateGenerator.generate(code_parser_state, options
+      |> Keyword.put_new(:extension, ".cs")
+      |> Keyword.put_new(:class_template, @unity_class_template)
+      |> Keyword.put_new(:enum_template, @enum_template)
+      |> Keyword.put_new(:enum_property_template, @enum_property_template)
+      |> Keyword.put_new(:interface_template, @interface_template)
+      |> Keyword.put_new(:interface_method_template, @interface_method_template)
+      |> Keyword.put_new(:interface_property_template, @interface_property_template)
+      |> Keyword.put_new(:method_template, @method_template)
+      |> Keyword.put_new(:method_parameter_doc_template, @method_parameter_doc_template)
+      |> Keyword.put_new(:public_property_template, @public_property_template)
+      |> Keyword.put_new(:private_property_template, @unity_private_property_template)
     )
   end
 
